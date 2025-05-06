59ce368bc5c0f936fa122c0d287147104ab4b9ea
beiliang_you <youbl@126.com>
2025-05-06 11:45:14 +0800

    增加feign全局日志

--- beinet-core/beinet-feign/src/main/java/cn/beinet/core/feign/FeignConfig.java
+++ beinet-core/beinet-feign/src/main/java/cn/beinet/core/feign/FeignConfig.java
@@ -1,9 +1,24 @@
 package cn.beinet.core.feign;
 
+import cn.beinet.core.feign.log.MyFeignLogger;
+import feign.Logger;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableFeignClients(basePackages = {"cn.beinet"})
 public class FeignConfig {
+    @Bean
+    Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
+    }
+
+    /**
+     * 使用自定义的日志记录器
+     */
+    @Bean
+    MyFeignLogger createMyLogger() {
+        return new MyFeignLogger();
+    }
 }
